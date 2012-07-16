31819753048a2114aa9dcfd46c320bcbe43f8537
johan <johan.grufman@mirendo.se>
2012-07-16 11:24:09 +0200

    Changed license identation

--- convx-core/src/test/java/org/convx/format/DateFormatTest.java
+++ convx-core/src/test/java/org/convx/format/DateFormatTest.java
@@ -1,17 +1,17 @@
-/**
- *     Copyright (C) 2012 Johan Grufman
- *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+/*
+   Copyright (C) 2012 Johan Grufman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 package org.convx.format;
 
@@ -24,8 +24,6 @@ import static org.junit.Assert.assertEquals;
  * @since 2012-04-05
  */
 public class DateFormatTest {
-
-
     @Test
     public void testParsing() throws Exception {
         DateFormat dateFormat = new DateFormat("dd/MM/yyyy");
